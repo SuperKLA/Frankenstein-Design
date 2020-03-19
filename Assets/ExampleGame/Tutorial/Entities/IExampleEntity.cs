ddc8e75c1593a4dd3b14ce931c09e2443bcf1dda
konstantin lapin <konstantinlapin@outlook.com>
2020-03-19 11:08:55 +0100

    Added some Example Files

---
+++ Assets/ExampleGame/Tutorial/Entities/IExampleEntity.cs
@@ -0,0 +1,37 @@
+﻿using Frankenstein;
+
+namespace ExampleGame.Entities
+{
+    public interface IExampleEntity : IAPIEntity<IExampleEntityService, IExampleEntityView>
+    {
+        /*
+         * Here we think about
+         * 
+         * what do I need to function?
+         * This is what the model has to fulfill
+         *
+         * Let's say this entity should calculate A + B or get the data from somewhere else
+         */
+        
+        float A { get; }
+        float B { get; }
+    }
+
+    public interface IExampleEntityService : IAPIEntityService
+    {
+        /*
+         * What does the entity do
+         * This is what the controller does
+         */
+        float Sum();
+    }
+
+    public interface IExampleEntityView : IAPIEntityView
+    {
+        /*
+         * The View is a door to connect view visual layer with other entities.
+         * it allows other entities to change this value. e.g. A figure exposes his transform
+         * to a move entity to give away this responsibility
+         */
+    }
+}
